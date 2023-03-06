0b5fe946bf93a46c61ca3a87dcd60b4b618c91d9
Rafael Franca Scofield Lauar <rafavasco12@hotmail.com>
2023-03-06 20:15:47 -0300

    Background adaptação
    Background e ground se adaptando ao tamanho da tela

---
+++ HatGame/Assets/_Game/Scripts/Ground.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ground : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpriteRenderer sprite = GetComponentInChildren<SpriteRenderer>();
+        Vector3 scaleTemp = GetComponentInChildren<Transform>().transform.localScale;
+
+        float width = sprite.bounds.size.x;
+        float heightCamera = Camera.main.orthographicSize * 2.15f;
+        float widthWorld = heightCamera / Screen.height * Screen.width;
+
+        scaleTemp.x = widthWorld / width;
+
+        transform.localScale = scaleTemp;
+    }
+}
