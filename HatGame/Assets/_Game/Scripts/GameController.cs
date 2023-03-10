e9b456cd8cede47272ff6061508fa7fdda20b0a4
Rafael Franca Scofield Lauar <rafavasco12@hotmail.com>
2023-03-10 17:48:14 -0300

    Menu Update
    Adicionado Menu de pausa e de game over

--- HatGame/Assets/_Game/Scripts/GameController.cs
+++ HatGame/Assets/_Game/Scripts/GameController.cs
@@ -20,7 +20,11 @@ public class GameController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        CountDownTime();
+        if(gameStarted)
+        {
+            CountDownTime();
+        }
+        
     }
 
     public void CountDownTime()
